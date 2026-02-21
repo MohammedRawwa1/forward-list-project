1779aecf293ef0ba96ec68abde392cd90ee79059
MohammedRawwa1 <84639956+MohammedRawwa1@users.noreply.github.com>
2026-02-21 22:24:35 +0200

    Update conversation_states.py

--- conversation_states.py
+++ conversation_states.py
@@ -3,6 +3,7 @@
 ADD_NAME, ADD_LINK, ADD_CATEGORY = range(3)
 # ---------------  /create_category  ---------------
 CREATE_CAT_NAME = 10
+CREATE_CAT_PARENT = 11
 # ---------------  /delete_all  ---------------
 DELETE_ALL, CONFIRM_DELETE, CANCEL_DELETE = range(20, 23)
 MAX_CATEGORY_NAME_LENGTH = 30
